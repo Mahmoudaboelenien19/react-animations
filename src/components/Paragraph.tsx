355fd0324f81a043bc58e4774c1f586b56b5b2b2
mahmoud.aboelenien <mahmoud.aboelenien@sdtgroup.co>
2025-06-08 18:37:23 +0300

    2

--- src/components/Paragraph.tsx
+++ src/components/Paragraph.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Paragraph = () => {
   return (
     <p
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,8 +1,7 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Paragraph from "../../components/Paragraph";
 
 const About = () => {
-  const navigate = useNavigate();
   return (
     <>
       <br />
